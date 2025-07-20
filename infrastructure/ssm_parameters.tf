6a5418ff25bea9781980bce3acccd979148adaf8
gorstak17 <loncar.aco@gmail.com>
2025-07-20 20:21:23 +0200

    use SecureString

--- infrastructure/ssm_parameters.tf
+++ infrastructure/ssm_parameters.tf
@@ -6,7 +6,7 @@ resource "aws_ssm_parameter" "app_key" {
 
 resource "aws_ssm_parameter" "redis_endpoint" {
   name      = "/laravel-counter/redis_endpoint"
-  type      = "String"
+  type      = "SecureString"
   value     = module.redis.redis_primary_endpoint
   overwrite = true
 }
